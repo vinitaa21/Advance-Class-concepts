477a45a4943ae53b312fe7ed8c475f55e8c8eb53
vinitaa21 <vinitaanaik@gmail.com>
2026-07-17 12:21:43 +0530

    Add files via upload

---
+++ Advance_class_concepts.py
@@ -0,0 +1,64 @@
+# def my_decorator(func):
+#     def wrapper():
+#         print("Something is happening before the function is called.")
+#         func()
+#         print("Something is happening after the function is called.")
+#     return wrapper
+# @my_decorator
+# def say_hello():
+#     print("Hello!")
+# say_hello()
+
+#OUTPUTS :--
+# Something is happening before the function is called.
+# Hello!
+# Something is happening after the function is called.
+
+
+
+# my_list=[1,2,3,4,5]
+# my_iter=iter(my_list)
+# print(next(my_iter))
+# print(next(my_iter))
+# print(next(my_iter))
+
+#OUTPUTS:--
+# 1
+# 2
+# 3
+
+
+
+def countdown(n):
+    while n>0:
+        yield n
+        n-=1
+for num in countdown(5):
+    print(num)
+        
+#OUTPUTS:--
+1
+2
+3
+5
+4
+3
+2
+1
+        
+
+        
+def outer_func(x):
+    def inner_func(y):
+        return x+y
+    return inner_func
+add_five=outer_func(5)
+print(add_five(3))
+
+#OUTPUT:--
+5
+4
+3
+2
+1
+8
